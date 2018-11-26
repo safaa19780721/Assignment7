ce928ab40ab831987ddcb222cd8222117f04026e
ricky <ricky@10.80.147.248>
2018-11-26 13:51:52 -0500

    .

--- Assignment7/src/ICourse.java
+++ Assignment7/src/ICourse.java
@@ -1,4 +1,14 @@
 
 public interface ICourse {
-
+	// Determine if the capacity for this course has been reached.
+	boolean isFull();
+	
+	// Add this student to the arraylist for enrolled students for this course.
+	void addStudent(String name);
+	
+	// Print the classlist for this course.
+	void printClassList();
+	
+	// Determines if this course object is the same as the object received as parameter.
+	boolean equals(Object arg0);
 }
--- Assignment7/src/INode.java
+++ Assignment7/src/INode.java
@@ -1,4 +1,10 @@
-
-public interface INode {
-
+public interface INode<E> {
+	// Returns the data stored in this node.
+	E getData();
+	
+	// Returns the node next to this node.
+	Node<E> getNext();
+	
+	// Sets node received as the next node to this node.
+	void setNext(Node<E> next);
 }
