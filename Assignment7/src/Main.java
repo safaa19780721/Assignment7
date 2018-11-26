c73667c6fb51edb5de205cd9a77f430e14408c00
ricky <ricky@10.80.147.248>
2018-11-26 13:35:32 -0500

    Ricky: Main info.

--- Assignment7/src/Main.java
+++ Assignment7/src/Main.java
@@ -1,8 +1,19 @@
+import java.util.*;
+import java.io.*;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException{
+		
+		PriorityQueue<Request> requestQueue = new PriorityQueue<Request>();
+		ArrayList<Course> courses = new ArrayList<Course>();
+		BufferedReader fileIn = new BufferedReader(new FileReader("course.txt"));
+		BufferedReader fileIn1 = new BufferedReader(new FileReader("request.txt"));
+		IController control = new Controller(requestQueue, courses, fileIn, fileIn1);
+		control.readCourseFile();
+		control.readRequestFile();
+		control.processRequests();
+		control.printClassList();
 
 	}
 
